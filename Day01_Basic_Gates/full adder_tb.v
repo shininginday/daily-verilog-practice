4cfa3100a678de2efd2e2f7f04f1d868b94237b2
Brunda Raj G K <121303695+shininginday@users.noreply.github.com>
2026-06-09 08:33:09 +0530

    added full adder testbench

---
+++ Day01_Basic_Gates/full adder_tb.v
@@ -0,0 +1,33 @@
+module fa_tb;
+  reg a,b,cin;
+  wire s,c;
+  
+  //mod dut instantiation
+  fa dut(a,b,cin,s,c);
+  
+  //stimuli generation
+  initial begin
+    a=0; b=0; cin=0;
+    #2 a=0; b=0; cin=1;
+    #2 a=0; b=1; cin=0;
+    #2 a=0; b=1; cin=1;
+    #2 a=1; b=0; cin=0;
+    #2 a=1; b=0; cin=1;
+    #2 a=1; b=1; cin=0;
+    #2 a=1; b=1; cin=1;
+  end
+   //monitoring of inputs and outputs
+  
+ initial begin
+   $monitor("a=%b, b=%b, cin=%b, sum=%b, carry=%b",a,b,cin,s,c);
+ end
+  
+  //dump file and dump vars for waveform
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,a,b,cin,s,c);
+  end
+  
+  
+endmodule
+  
