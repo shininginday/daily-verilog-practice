9166adf09bea40ccae2d36160996a391e9a65920
Brunda Raj G K <121303695+shininginday@users.noreply.github.com>
2026-06-09 10:10:12 +0530

    Add testbench for NAND gate

---
+++ Day01_Basic_Gates/nand_g_tb.v
@@ -0,0 +1,29 @@
+// Code your testbench here
+// or browse Examples
+module nand_g_tb;
+  reg a,b;
+  wire y;
+  
+  //mod dut instantiation
+  nand_g dut(a,b,y);
+  
+  //stimuli generation
+  initial begin
+    a=0; b=0;
+    #2 a=0; b=1;
+    #2 a=1; b=0;
+    #2 a=1; b=1;
+  end
+   //monitoring of inputs and outputs
+  
+ initial begin
+   $monitor("a=%b, b=%b, out=%b",a,b,y);
+ end
+  
+  //dump file and dump vars for waveform
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,a,b,y);
+  end  
+endmodule
+  
