66879e6871059df523e065f12f69b6a06e8baa97
Brunda Raj G K <121303695+shininginday@users.noreply.github.com>
2026-06-09 09:59:43 +0530

    Add testbench for OR gate functionality
    Implemented a testbench for the OR gate with stimuli and waveform dumping.

---
+++ Day01_Basic_Gates/or_gate_tb.v
@@ -0,0 +1,30 @@
+// Code your testbench here
+// or browse Examples
+module or_g_tb;
+  reg a,b;
+  wire y;
+  
+  //mod dut instantiation
+  or_g dut(a,b,y);
+  
+  //stimuli generation
+  initial begin
+    a=0; b=0;
+    #2 a=0; b=1;
+    #2 a=1; b=0;
+    #2 a=1; b=1;
+  end
+   //monitoring of inputs and outputs
+  
+ initial begin
+   $monitor("a=%b, b=%b, out=%b",a,b,y);
+ end
+  
+  //dump file and dump vars for waveform
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,a,b,y);
+  end
+  
+  
+endmodule
