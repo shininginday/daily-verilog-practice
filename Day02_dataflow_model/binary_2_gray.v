0641c3d4d5937c25de6d1bc115b39b1da17f2f74
Brunda Raj G K <121303695+shininginday@users.noreply.github.com>
2026-06-10 10:04:25 +0530

    Add 3-bit binary to Gray code conversion module
    Implemented a module for converting 3-bit binary to Gray code.

--- Day02_dataflow_model/binary_2_gray.v
+++ Day02_dataflow_model/binary_2_gray.v
@@ -1,1 +1,9 @@
-
+//design code of 3bit binary to gray conversion
+module b2g(b,g);
+  input [2:0]b;
+  output [2:0]g; 
+  //binary to gray conversion code
+  assign g[2] = b[2];
+  assign g[1] = b[2]^b[1];
+  assign g[0] = b[1]^b[0];  
+endmodule;
