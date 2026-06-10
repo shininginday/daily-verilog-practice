bb766340c3e0d43dd35847a91bf0fac66228b2c5
Brunda Raj G K <121303695+shininginday@users.noreply.github.com>
2026-06-10 08:56:47 +0530

    Refactor mux2to1 to use a NOT gate for selection

--- Day01_Basic_Gates/2:1mux.v
+++ Day01_Basic_Gates/2:1mux.v
@@ -4,9 +4,9 @@
 module mux2to1(in1,in2,s,out);
   input in1,in2,s; //input
   output out;  //output
-  wire w1,w2;
-  
-  and a1(w1,~s,in1); // indicating first input when s=0
+  wire w1,w2,w3;
+  not n1(w3,s);
+  and a1(w1,w3,in1); // indicating first input when s=0
   and a2(w2,s,in2); //indicating second input when s=1
   or o1(out,w1,w2);
   
