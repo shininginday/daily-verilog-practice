9e0ec9425cbb2d57a70c85975a2624aa9abee045
Brunda Raj G K <121303695+shininginday@users.noreply.github.com>
2026-06-11 11:05:08 +0530

    Create design.v

---
+++ Day_03_Behavioural model/sequential ckt/latch/d_latch/d latch with reset asynchronous/design.v
@@ -0,0 +1,13 @@
+module dlatch(d,clk,rst,q);
+  input d,clk,rst;
+  output reg q;
+  
+  always @(*)
+    begin 
+      if(rst)
+        q=0;
+      else
+        if(clk)
+          q=d;  
+    end
+endmodule
