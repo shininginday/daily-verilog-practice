f189171b7e10b86383f86aee1dff6ad7c5a15fa4
Brunda Raj G K <121303695+shininginday@users.noreply.github.com>
2026-06-15 08:54:05 +0530

    Add testbench for dlatch with clock generation

---
+++ Day04_sequentialckt/clock genration/dlatch with clock genration/clockgen_tb.v
@@ -0,0 +1,35 @@
+// or browse Examples
+module dlatch_tb;
+  reg d,clk,rst;
+  wire q;
+
+  //dut instantiation
+  dlatch dut(.d(d),.clk(clk),.rst(rst),.q(q));
+  
+  initial begin
+    clk = 0;
+    rst = 0; d=0; //active low
+    #11 rst =1;
+    #5 d=1;
+    #18 d=0;
+    #20 d=1;
+    #5 d=0;
+    #10 d=1;
+    #100 $finish;
+  end
+  always #5 clk=~clk;
+    
+  
+//monitoring of input and output
+  
+  initial begin
+    $monitor("d=%b, clk=%b, q=%b, rst=%b",d,clk,q,rst);
+  end
+  
+  //waveform
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(0,d,clk,q,rst);
+  end
+  
+endmodule
