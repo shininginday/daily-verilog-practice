09fe95c68921b4281f23339b6f24de735f5d6968
Brunda Raj G K <121303695+shininginday@users.noreply.github.com>
2026-06-18 18:41:25 +0530

    Create design.v

---
+++ day_07_verilog practice/functions/clk frequency division/30% duty cycle/design.v
@@ -0,0 +1,32 @@
+// Code your design here
+module freq_div(input clk_in,rst, output reg clk_out);
+  
+  integer count;
+  reg temp_clk;
+  
+  always @(posedge clk_in or negedge clk_in)
+    begin
+      if(rst)
+        begin
+            count=0;
+            temp_clk =0;
+        end
+      else 
+        count = count+1;
+      if(count<=7)
+        begin
+           temp_clk = 0;
+        end
+      else
+        if (count>7 && count<=10)
+          begin
+            temp_clk=1;
+          end
+      else 
+        begin
+        count = 0;
+         end
+        clk_out=temp_clk;
+        
+    end
+endmodule
