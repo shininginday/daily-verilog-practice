dfc35f1a6f644cd7c2f2677fd0ca9464c57f7bd9
Brunda Raj G K <121303695+shininginday@users.noreply.github.com>
2026-06-18 20:09:56 +0530

    Create design.v

---
+++ day_07_verilog practice/functions/clk frequency division/using task function/design.v
@@ -0,0 +1,39 @@
+module freq_div(input clk_in,rst, output reg clk_out);
+  
+  integer count;
+  reg temp_clk;
+  
+  always @(posedge clk_in or negedge clk_in)
+    begin
+      fd();    
+    end
+
+  task fd();
+     if(rst)
+        begin
+            count=0;
+            temp_clk =0;
+        end
+      else 
+        count = count+1;
+      if(count%3==0)
+        begin
+           temp_clk = ~temp_clk;
+        end
+      else
+        if (count>5)
+          begin
+        count = 0;
+         end
+        clk_out=temp_clk;
+    endtask
+ endmodule
+      
+      
+    
+      
+       
+       
+      
+      
+      
